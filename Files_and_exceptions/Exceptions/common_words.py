d5d1c8b096c674bc13426dff9df296904f00b460
Anton Durandin <enitefall81@gmail.com>
2022-07-07 13:25:09 -0700

    Word occurrences in books analyzer

---
+++ Files_and_exceptions/Exceptions/common_words.py
@@ -0,0 +1,15 @@
+books = ['mobi.txt', 'siddharta.txt', 'alice.txt']
+
+def occurrences_calculator(sources, word):
+    occurrences_counter = 0
+    for source in sources:
+        try:
+            with open(source) as file_object:
+                content = file_object.read()
+        except FileNotFoundError:
+            pass
+        else:
+            occurrences_counter += content.count(word)
+    print(occurrences_counter)
+
+occurrences_calculator(books, 'magnificent')
