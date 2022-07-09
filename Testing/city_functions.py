38c5741914ac00151c178a0b6534ce2356eb2a16
Anton Durandin <enitefall81@gmail.com>
2022-07-09 12:01:43 -0700

    2 test with 3rd argument

--- Testing/city_functions.py
+++ Testing/city_functions.py
@@ -1,4 +1,8 @@
-def city_country(city, country):
-    formatted_string = f"{city}, {country}"
+def city_country(city, country, population=''):
+    if population:
+        formatted_string = f"{city}, {country} population - {population}"
+    else:
+        formatted_string = f"{city}, {country}"
     capitalized = formatted_string.title()
     return capitalized
+
--- Testing/test_cities.py
+++ Testing/test_cities.py
@@ -1,10 +1,15 @@
 import unittest
 from city_functions import city_country
 
+
 class CountryTestCase(unittest.TestCase):
     def test_city_country(self):
         formatted_city_country = city_country('chile', 'santiago')
         self.assertEqual(formatted_city_country, 'Chile, Santiago')
+
+    def test_city_country_population(self):
+        formatted_city_country = city_country('chile', 'santiago', '30000')
+        self.assertEqual(formatted_city_country, 'Chile, Santiago Population - 30000')
 
 if __name__ == '__main__':
     unittest.main()
