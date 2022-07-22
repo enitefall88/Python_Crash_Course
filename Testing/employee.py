83bd8787eed85a68fd3afac3844b1be96fb95fa5
Anton Durandin <enitefall81@gmail.com>
2022-07-22 10:02:14 -0700

    Testing

---
+++ Testing/employee.py
@@ -0,0 +1,15 @@
+class Employee:
+    def __init__(self, first_name, last_name, annual_salary):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.annual_salary = annual_salary
+
+    def give_raise(self, raise_ammount=5000):
+        self.annual_salary += raise_ammount
+
+    def show_salary(self):
+        print(self.annual_salary)
+
+john = Employee('John', 'Johnson', 50000)
+john.give_raise(3000)
+john.show_salary()
